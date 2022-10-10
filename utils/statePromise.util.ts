666f4b6c2d9aaf420dd5b947bc03d2a368075cba
Alexander Khalivetov <Alexander.Khalivetov@waveaccess.global>
2022-10-10 12:31:37 +0600

    Убрано лишнее

--- utils/statePromise.util.ts
+++ utils/statePromise.util.ts
@@ -2,9 +2,7 @@ export class StatePromise<ResolveData = unknown, RejectData = unknown> {
     private _promise: Promise<ResolveData>;
     private _resolve: (result: ResolveData) => void;
     private _reject: (reason: RejectData) => void;
-    private _result: ResolveData;
-    private _error: unknown;
-    private _isCompleted = false;
+    private _isCompleted = ref(false);
 
     constructor() {
         this._promise = new Promise<ResolveData>((resolve, reject) => {
@@ -14,32 +12,28 @@ export class StatePromise<ResolveData = unknown, RejectData = unknown> {
     }
 
     public resolve(result?: ResolveData): void {
-        if (this._isCompleted) {
-            return;
-        }
-        this._result = result;
         this._resolve(result);
         this.onComplete();
     }
 
     public reject(reason?: RejectData): void {
-        if (this._isCompleted) {
-            return;
-        }
-        this._error = reason;
         this._reject(reason);
         this.onComplete();
     }
 
     public then(handler: (result?: ResolveData) => unknown): void {
-        this._isCompleted ? handler(this._result) : this._promise.then(handler);
+        this._promise.then(handler);
     }
 
     public catch(handler: (error?: unknown) => unknown): void {
-        this._isCompleted ? handler(this._error) : this._promise.catch(handler);
+        this._promise.catch(handler);
+    }
+
+    public get isCompleted(): boolean {
+        return unref(this._isCompleted)
     }
 
     private onComplete(): void {
-        this._isCompleted = true;
+        this._isCompleted.value = true;
     }
 }
